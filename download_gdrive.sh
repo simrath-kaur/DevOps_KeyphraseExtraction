3e28f4ae98659a55337088fabd9b06283fd82616
simrath-kaur <160116417+simrath-kaur@users.noreply.github.com>
2024-05-23 18:32:51 +0530

    Update download_gdrive.sh

--- download_gdrive.sh
+++ download_gdrive.sh
@@ -1,13 +1,24 @@
+# #!/bin/bash
+
+# FILE_ID=$1
+# DESTINATION=$2
+
+# # Create cookies file to store the confirmation token
+# curl -sc /tmp/gcookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
+
+# # Extract the confirmation token
+# CONFIRM=$(awk '/download/ {print $NF}' /tmp/gcookie)
+
+# # Download the file using the confirmation token
+# curl -Lb /tmp/gcookie "https://drive.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" -o ${DESTINATION}
+
 #!/bin/bash
 
 FILE_ID=$1
-DESTINATION=$2
+FILE_NAME=$2
 
-# Create cookies file to store the confirmation token
-curl -sc /tmp/gcookie "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
+# Fetching the confirmation token
+CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=${FILE_ID}" -O- | sed -rn 's/.confirm=([0-9A-Za-z_]+).*/\1\n/p')
 
-# Extract the confirmation token
-CONFIRM=$(awk '/download/ {print $NF}' /tmp/gcookie)
-
-# Download the file using the confirmation token
-curl -Lb /tmp/gcookie "https://drive.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" -o ${DESTINATION}
+# Downloading the file using the confirmation token
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILE_ID}" -O ${FILE_NAME} && rm -rf /tmp/cookies.txt
